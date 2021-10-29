9236271342855ef2eaf53437a7a7872de17cb800
soxrok2212 <soxrok2212@gmail.com>
2021-10-29 00:38:28 -0700

    more

--- src/networks.py
+++ src/networks.py
@@ -67,14 +67,14 @@ class Network():
 
     def probe_network(self, interface, interface_mac):
         src = mac2str(interface_mac)
-        dst = mac2str(bssid)
+        dst = mac2str(self.bssid)
         change_channel(interface, self.channel)
 
         pkt = RadioTap()/Dot11(addr1=dst, addr2=src, addr3=src)\
             /Dot11ProbeReq()\
             /Dot11Elt(ID=0, info=self.essid)\
             /Dot11Elt(ID=1, info='\x82\x84\x8b\x96\x0c\x12\x18')\
-            /Dot11Elt(ID=50, info='\x30\x48\x60\x6b')\
+            /Dot11Elt(ID=50, info='\x30\x48\x60\x6c')\
             /Dot11Elt(ID=3, info=chr(self.channel))
         answer = srp1(pkt, iface=interface, verbose=0)
         answer.show()
